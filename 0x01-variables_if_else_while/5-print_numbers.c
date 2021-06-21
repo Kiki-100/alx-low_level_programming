cea3fa3bfc10e121ebc4bb9346e662e8f2720596
Kiki-100 <nyaga6178@gmail.com>
2021-06-20 20:48:57 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This program program that
+ * Description: prints numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+int number;
+
+for (number = 0; number <= 9; number++)
+printf("%d", number);
+printf("\n");
+
+return (0);
+}
