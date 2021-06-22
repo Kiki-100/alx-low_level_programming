307c52c498ba2622292c269a76328f05e9029ff0
Kiki-100 <nyaga6178@gmail.com>
2021-06-22 05:49:31 -0700

    lower case alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+  #include "holberton.h"
+/**
+ * main - Entry point for printfunc
+*tion /prints alphabet
+*print_alphabet- prints alphabet in lowercase. 
+*returns void 
+*
+*return- main function returns an intof 0
+*/
+int main(void)
+{
+char ch;
+void print_alphabet(void);
+
+for (ch = 'a' ; ch = 'z' ; ch++)
+
+{
+_putchar(ch);
+print_alphabet();
+}
+return (0);
+}
+
+
+
