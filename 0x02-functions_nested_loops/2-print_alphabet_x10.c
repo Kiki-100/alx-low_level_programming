4e53ce98d1a2499dfae434f3d841a0ecd5c42cac
Kiki-100 <nyaga6178@gmail.com>
2021-06-23 01:19:02 -0700

    script changes

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,21 +10,17 @@ void print_alphabet_x10(void)
 {
 char c;
 int i;
- 
+
 i = 0;
 while (i < 10)
 {
 c = 'a';
-
 while (c <= 'z')
-
 {
 _putchar(c);
- c++;
-
+c++;
 }
 _putchar('\n');
 i++;
 }
 }
-
