1673798a887747843a3ac00b1541deff371d790e
Kiki-100 <nyaga6178@gmail.com>
2021-06-28 04:45:01 -0700

    changes

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - function to print integer 
+* @n: shows integer
+*/
+void print_number(int n);
+{
+if (n < 0)  
+{
+_putchar('-');
+n = - n
+}
+int main(void)
+{
+int n = 12;
+print (n);
+return (0);
+}
+ 
+
+		
