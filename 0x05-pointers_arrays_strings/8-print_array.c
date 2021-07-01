bd311153e157924cb94da4711139714c33130343
Kiki-100 <nyaga6178@gmail.com>
2021-07-01 01:39:08 -0700

    task 8 added

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_array - prints all elementsof array
+* @a: the array
+* @n: the array length
+*
+* Return: void
+*/
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i + 1 < n)
+printf(", ");
+}
+printf("\n");
+}
+
