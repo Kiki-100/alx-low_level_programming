94cf873d1de862a3780b4f6cdd658e599d7f030b
Kiki-100 <nyaga6178@gmail.com>
2021-07-01 00:41:44 -0700

    added task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton"
+
+/**
+ * swap_int - swaps the values of a and b
+ * @a: the first integer
+ * @b: the second integer
+ *
+ * Return: void
+*/
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
