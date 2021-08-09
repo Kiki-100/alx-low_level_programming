794dfdc4fae480a1d53f98e81e22cb92c7bb04dc
Kiki-100 <nyaga6178@gmail.com>
2021-08-09 06:22:11 -0700

    task

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+  #include <stdio.h>
+
+/**
+ * append_text_to_file - appends text to a file
+ *
+ * @filename: name of file
+ * @text_content: content to append
+ * Return: Returns 1 on success and -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int file, i;
+
+file = open(filename, O_RDWR | O_APPEND);
+if (filename == NULL)
+return (-1);
+if (file == -1)
+return (-1);
+if (text_content == NULL)
+return (1);
+i = 0;
+while (text_content[i] != '\0')
+i++;
+file = write(file, text_content, i);
+if (file == -1)
+return (-1);
+return (1);
+}
